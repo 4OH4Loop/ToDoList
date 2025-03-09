c764709554099a3662dd5e5fbac72aec205ba1dd
4OH4Loop <semperchuck@gmail.com>
2025-03-09 16:54:25 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Carolyn Ballinger on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
