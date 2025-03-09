ae2115cdfabf294b3b57332ef32470af01d70b8a
4OH4Loop <semperchuck@gmail.com>
2025-03-09 18:43:30 -0400

    Ch 5.2 Updates

--- ToDoList/ToDoListView.swift
+++ ToDoList/ToDoListView.swift
@@ -8,21 +8,64 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    var toDos = ["Learn Swift",
+                 "Change the World",
+                 "Bring the Awesome",
+                 "Take a Vacation"]
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                NavigationLink {
-                    DetailView()
-                }label: {
-                    Image(systemName: "eye")
-                    Text("Show the New View!")
+            List {
+                ForEach(toDos, id: \.self) {toDo in
+                    NavigationLink {
+                        DetailView(passedValue: toDo)
+                    } label: {
+                        Text(toDo)
+                    }
                 }
-                .buttonStyle(.borderedProminent)
             }
-            .padding()
+            .navigationTitle("To Do List")
+            .navigationBarTitleDisplayMode(.automatic)
+            .listStyle(.plain)
+            //                ForEach(0..<100, id: \.self) { number in
+            //                    NavigationLink {
+            //                        DetailView(passedValue: "Item \(number)")
+            //                    } label: {
+            //                        Text("Item \(number+1)")
+            //                    }
+            //                }
+            //                Section {
+            //                    NavigationLink {
+            //                        DetailView()
+            //                    } label: {
+            //                        Text ("Winter")
+            //                    }
+            //                    Text("Summer")
+            //                } header: {
+            //                    Text("Breaks")
+            //                }
+            //                Section {
+            //                    Text("Spring")
+            //                    Text("Fall")
+            //                } header: {
+            //                    Text("Semesters")
+            //                }
+            
+            
         }
+        
+        //        VStack {
+        //            NavigationLink {
+        //                DetailView()
+        //            }label: {
+        //                Image(systemName: "eye")
+        //                Text("Show the New View!")
+        //            }
+        //            .buttonStyle(.borderedProminent)
+        //        }
+        //        .padding()
     }
 }
-    #Preview {
-        ToDoListView()
-    }
+#Preview {
+    ToDoListView()
+}
